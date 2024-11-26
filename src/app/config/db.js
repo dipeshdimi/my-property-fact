1ebf73977f5b62b3680531067e57c394de5e97ad
Abhishek Kumar <abhishek8601679@gmail.com>
2024-11-26 18:06:40 +0530

    ip

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "CTM.CO.IN",
+    host: "118.139.179.25",
     user: "rmw_my_property_fact",
     password: "G;_n}*QMy9XZ",
     database: "my-property-fact"
